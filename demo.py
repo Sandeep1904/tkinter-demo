36bcb144696fcf4fc9727460acc18c4ebae8c78c
Sandeep Hugar <sandeephugar19@gmail.com>
2020-02-10 06:54:00 +0530

    used frames in window

--- demo.py
+++ demo.py
@@ -1,9 +1,18 @@
 from tkinter import *
 # add window by creating object of tkinter class
 root = Tk()
-# create labels
-label1 = Label(root, text="hello Sandeep!")
-# add labels to the window
-label1.pack()
-# closes only when cross button is clicked
+# create frame object
+
+newframe = Frame(root)
+newframe.pack()
+
+otherframe = Frame(root)
+otherframe.pack(side = BOTTOM)
+
+button1 = Button(newframe,text="click here", fg="red")
+button2 = Button(newframe,text="click here", fg="blue")
+
+button1.pack()
+button2.pack()
+
 root.mainloop()
