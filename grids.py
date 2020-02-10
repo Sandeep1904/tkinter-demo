060608cf8540df1493756bc1726754e93427c0d5
Sandeep Hugar <sandeephugar19@gmail.com>
2020-02-10 07:01:51 +0530

    used grid system

---
+++ grids.py
@@ -0,0 +1,16 @@
+from tkinter import *
+root = Tk()
+
+
+label1 = Label(root,text='Fname')
+label2 = Label(root,text='Lname')
+
+entry1 = Entry(root)
+entry2 = Entry(root)
+
+label1.grid(row=0, column=0)
+label2.grid(row=1, column=0)
+
+entry1.grid(row=0,column=1)
+entry2.grid(row=1,column=1)
+root.mainloop()
