f0839f81a044fa1662366c21a9b2b13ce39864a1
Sandeep Hugar <sandeephugar19@gmail.com>
2020-02-10 07:12:46 +0530

    self adjusting labels

--- grids.py
+++ grids.py
@@ -2,8 +2,13 @@ from tkinter import *
 root = Tk()
 
 
-label1 = Label(root,text='Fname')
-label2 = Label(root,text='Lname')
+label1 = Label(root,text='Fname', bg='red', fg='white')
+label2 = Label(root,text='Lname',bg='green',fg = 'white')
+
+# if you want to make self adjusting items
+# label1.pack(fill=X)
+# label2.pack(side=LEFT,fill=Y)
+
 
 entry1 = Entry(root)
 entry2 = Entry(root)
