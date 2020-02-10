ec1da683e3d27f5891191a64fa6fac6bea747ffb
Sandeep Hugar <sandeephugar19@gmail.com>
2020-02-10 06:48:56 +0530

    initial commit

--- demo.py
+++ demo.py
@@ -1,9 +1,9 @@
 from tkinter import *
-
+# add window by creating object of tkinter class
 root = Tk()
-
+# create labels
 label1 = Label(root, text="hello Sandeep!")
-
+# add labels to the window
 label1.pack()
-
+# closes only when cross button is clicked
 root.mainloop()
